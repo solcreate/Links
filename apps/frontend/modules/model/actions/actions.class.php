01d847cfb0c0fec7f1c7e76dfbd2c53086b51349
kawahara <solcreate@gmail.com>
2013-03-10 23:48:33 +0900

    first commit

---
+++ apps/frontend/modules/model/actions/actions.class.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * model actions.
+ *
+ * @package    Links
+ * @subpackage model
+ * @author     Your name here
+ * @version    SVN: $Id: actions.class.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class modelActions extends sfActions
+{
+ /**
+  * Executes index action
+  *
+  * @param sfRequest $request A request object
+  */
+  public function executeIndex(sfWebRequest $request)
+  {
+    $this->forward('default', 'module');
+  }
+}
